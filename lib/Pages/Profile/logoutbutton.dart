e336d6906ad8e56a69f4e4283422d4cb7fe492fd
Yash05080 <yash05080@gmail.com>
2024-07-30 18:27:22 +0530

    login button shift

---
+++ lib/Pages/Profile/logoutbutton.dart
@@ -0,0 +1,17 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class LogOutButton extends StatelessWidget {
+  const LogOutButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      onPressed: () {
+        FirebaseAuth.instance.signOut();
+      },
+      color: Colors.red,
+      child: Text("log out"),
+    );
+  }
+}
